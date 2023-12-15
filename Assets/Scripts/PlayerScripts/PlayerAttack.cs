ae4df95cfcd34cdb81bed2f13201305a2cc1cc1b
o1sho <vityakobzev1234@gmail.com>
2023-12-15 19:39:05 +0700

    ver. 0.0.7
    upgrade Attack system

--- Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -13,6 +13,12 @@ public class PlayerAttack : MonoBehaviour
     private InputManager _inputManager;
 
     private Vector2 _attackDirection;
+    [SerializeField] Transform attackPointLeft;
+    [SerializeField] Transform attackPointRight;
+    [SerializeField] Transform attackPointUp;
+    [SerializeField] Transform attackPointDown;
+    public float attackRange;
+    [SerializeField] LayerMask enemyLayers;
 
     private void Awake()
     {
@@ -39,21 +45,70 @@ public class PlayerAttack : MonoBehaviour
         {
             Debug.Log(_attackDirection);
             Debug.Log("LeftAttack!");
+
+            Collider2D[] hitEnemies  = Physics2D.OverlapCircleAll(attackPointLeft.position, attackRange, enemyLayers);
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("Hit enemy " + enemy.name);
+            }
         }
         if (_attackDirection == new Vector2(1, 0))
         {
             Debug.Log(_attackDirection);
             Debug.Log("RightAttack!");
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPointRight.position, attackRange, enemyLayers);
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("Hit enemy " + enemy.name);
+            }
         }
         if (_attackDirection == new Vector2(0, 1))
         {
             Debug.Log(_attackDirection);
             Debug.Log("UpAttack!");
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPointUp.position, attackRange, enemyLayers);
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("Hit enemy " + enemy.name);
+            }
         }
         if (_attackDirection == new Vector2(0, -1))
         {
             Debug.Log(_attackDirection);
             Debug.Log("DownAttack!");
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPointDown.position, attackRange, enemyLayers);
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("Hit enemy " + enemy.name);
+            }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (attackPointLeft == null)
+        {
+            return;
+        }
+        if (attackPointRight == null)
+        {
+            return;
+        }
+        if (attackPointUp == null)
+        {
+            return;
+        }
+        if (attackPointDown == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPointLeft.position, attackRange);
+        Gizmos.DrawWireSphere(attackPointRight.position, attackRange);
+        Gizmos.DrawWireSphere(attackPointUp.position, attackRange);
+        Gizmos.DrawWireSphere(attackPointDown.position, attackRange);
     }
 }
